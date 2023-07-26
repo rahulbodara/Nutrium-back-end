2c5c0f913554143b51506130cc4552c91446e5ab
bodararahul <bodararahul@gmail.com>
2023-07-26 09:40:25 +0530

    create Eating Behaviour API

---
+++ src/model/eatingBehaviour.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const eatingBehaviourSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'user',
+  },
+  clientId: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'Clients',
+  },
+  date: {
+    type: String,
+    required: [true, 'Please enter a date of birth'],
+    validate: {
+      validator: function (value) {
+        const dateRegex = /^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[0-2])\/\d{4}$/;
+        if (!dateRegex.test(value)) {
+          return false;
+        }
+        const parts = value.split('/');
+        const day = parseInt(parts[0], 10);
+        const month = parseInt(parts[1], 10);
+        const year = parseInt(parts[2], 10);
+        const date = new Date(year, month - 1, day);
+        return (
+          date.getDate() === day &&
+          date.getMonth() === month - 1 &&
+          date.getFullYear() === year
+        );
+      },
+      message: 'Please enter a valid date of birth (DD/MM/YYYY)',
+    },
+  },
+  behaviour: {
+    type: String,
+  },
+});
+
+module.exports = mongoose.model('EatingBehaviour', eatingBehaviourSchema);
