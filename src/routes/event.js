33097d6df87110846bbde1f908d726d1679ab1ec
rahulbodara <rb.globalia@gmail.com>
2023-09-26 09:07:03 +0530

    fix issue in evenet and delete event API

--- src/routes/event.js
+++ src/routes/event.js
@@ -6,5 +6,6 @@ const { isAuthenticated } = require("../middleware/auth");
 router.post("/event", isAuthenticated, eventController.createEvent);
 router.get("/event", isAuthenticated, eventController.getAllEvents);
 router.put('/event/:id', isAuthenticated,eventController.updateEvent);
+router.delete('/event/:id', isAuthenticated,eventController.deleteEvent);
 
 module.exports = router;
