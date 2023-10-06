57bdaa5ca3fee1842ee98d7bd6217cfd8154a4db
rahulbodara <rb.globalia@gmail.com>
2023-10-06 18:03:35 +0530

    fix issue createMeal,deleteMeal API and create deleteMealPlanObject

--- src/model/Mealplan.js
+++ src/model/Mealplan.js
@@ -3,40 +3,82 @@ const mongoose = require('mongoose');
 const mealPlanSchema = mongoose.Schema({
   userId: {
     type: mongoose.Schema.Types.ObjectId,
-    // required: true,
     ref: 'user',
   },
   clientId: {
     type: mongoose.Schema.Types.ObjectId,
-    // required: true,
     ref: 'Clients',
   },
-  name: String,
-  time: String,
-  foods: [
+  mealPlans: [
     {
       name: String,
-      subfoods: [
+      time: String,
+      foods: [
         {
           name: String,
+          subfoods: [
+            {
+              name: String,
+            },
+          ],
         },
       ],
+      Appetizer: [
+        {
+          name: String,
+          subfoods: [
+            {
+              name: String,
+            },
+          ],
+        },
+      ],
+      Dish: [
+        {
+          name: String,
+          subfoods: [
+            {
+              name: String,
+            },
+          ],
+        },
+      ],
+      Dessert: [
+        {
+          name: String,
+          subfoods: [
+            {
+              name: String,
+            },
+          ],
+        },
+      ],
+      Beverage: [
+        {
+          name: String,
+          subfoods: [
+            {
+              name: String,
+            },
+          ],  
+        },
+      ],
+      notes: String,
     },
   ],
-  notes: String,
   days: {
     type: [String],
     default: ['Every Day'],
   },
   creationMethod: {
     type: String,
-    enum: ['Merge selected days into a single version', 'Create a version for each day'],
-    default: 'Merge selected days into a single version',
+    enum: ['Merge selected days into a single version', 'Create a version for each day','single day'],
+    default: 'single day',
   },
   copyMealPlan: {
     type: [String],
     default: 'Do not copy',
-  },
+  }
 });
 
 module.exports = mongoose.model('Mealplan', mealPlanSchema);
--- src/routes/mealplan.js
+++ src/routes/mealplan.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { isAuthenticated } = require('../middleware/auth');
-const { createMealPlan,getMealPlan,updateMealPlan,deleteMealPlan } = require('../controller/mealplan');
+const { createMealPlan,getMealPlan,updateMealPlan,deleteMealPlan,deleteMealPlanObject } = require('../controller/mealplan');
 
 router.post('/meal-plan/:clientId', isAuthenticated, createMealPlan);
 
@@ -10,5 +10,7 @@ router.get('/getMeal-plan/:clientId', isAuthenticated, getMealPlan);
 router.put('/updateMeal-plan/:mealId', isAuthenticated, updateMealPlan);
 
 router.delete('/deleteMeal-plan/:mealId', isAuthenticated, deleteMealPlan);
+
+router.delete('/deleteMeal-planObject/:mealId/:objectId', isAuthenticated, deleteMealPlanObject);
 
 module.exports = router;
