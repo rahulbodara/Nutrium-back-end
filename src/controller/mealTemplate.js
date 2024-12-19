12f5191ce5156be7fc992475533fa11079d65b8b
rahulbodara <rb.globalia@gmail.com>
2024-12-19 14:45:32 +0530

    modefied create version api

--- src/controller/mealTemplate.js
+++ src/controller/mealTemplate.js
@@ -11,76 +11,9 @@ const {
   Supper,
 } = require("../model/mealTemplate");
 
-const createMealTemplate = async (req, res) => {
-  try {
-    const { name } = req.body;
-
-    if (!name)
-      return res.status(400).json({ message: "Meal plan name is required" });
-
-    const [breakfast, morningSnack, lunch, afternoonSnack, dinner, supper] =
-      await Promise.all([
-        Breakfast.create({}),
-        MorningSnack.create({}),
-        Lunch.create({}),
-        AfternoonSnack.create({}),
-        Dinner.create({}),
-        Supper.create({}),
-      ]);
-    const mealIds = [
-      breakfast._id,
-      morningSnack._id,
-      lunch._id,
-      afternoonSnack._id,
-      dinner._id,
-      supper._id,
-    ];
-    const mealPlanData = {
-      day: [
-        "Monday",
-        "Tuesday",
-        "Wednesday",
-        "Thursday",
-        "Friday",
-        "Saturday",
-        "Sunday",
-      ],
-      meals: mealIds,
-    };
-
-    const newMealPlan = await DaysMeal.create(mealPlanData);
-    const newDaysMeal = await MealPlan.create({
-      name,
-      mealSchedule: newMealPlan,
-    });
-
-    return res
-      .status(201)
-      .json({
-        message: "Meal Plan created successfully",
-        MealPlanTemplate: newDaysMeal,
-      });
-  } catch (error) {
-    console.error("Error creating Meal Plan:", error);
-    res.status(500).json({
-      message: "An error occurred while creating the Meal Plan",
-      error: error.message,
-    });
-  }
-};
-
-
-const createVersion = async (req, res) => {
-  try {
-    const templateId = "67628f3d1aec14f52daa8665";
-    const day = ["Saturday","Wednesday","Tuesday"]; 
-
-    const MealPlann = await MealPlan.findById(templateId).lean();
-    if (!MealPlann) {
-      return res.status(404).json({ message: "Meal Plan not found" });
-    }
-
-    const [breakfast, morningSnack, lunch, afternoonSnack, dinner, supper] = await Promise.all([
+const createMeals = async () => {
+  const [breakfast, morningSnack, lunch, afternoonSnack, dinner, supper] =
+    await Promise.all([
       Breakfast.create({}),
       MorningSnack.create({}),
       Lunch.create({}),
@@ -88,38 +21,76 @@ const createVersion = async (req, res) => {
       Dinner.create({}),
       Supper.create({}),
     ]);
+  
+  return [
+    breakfast._id,
+    morningSnack._id,
+    lunch._id,
+    afternoonSnack._id,
+    dinner._id,
+    supper._id,
+  ];
+};
 
-    const mealIds = [
-      breakfast._id,
-      morningSnack._id,
-      lunch._id,
-      afternoonSnack._id,
-      dinner._id,
-      supper._id,
-    ];
+const createMealTemplate = async (req, res) => {
+  try {
+    const { name } = req.body;
 
-    const updatedMealSchedules = MealPlann.mealSchedule.map(schedule => {
-      const updatedDays = schedule.day.filter(d => !day.includes(d)); 
-      return {
-        ...schedule,
-        day: updatedDays,
-      };
-    });
+    if (!name) {
+      return res.status(400).json({ message: "Meal plan name is required" });
+    }
 
-    const newMealPlan = {
-      day, 
+    const mealIds = await createMeals();
+    const mealPlanData = {
+      day: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
       meals: mealIds,
     };
-    const secondMealPlan = await DaysMeal.create(newMealPlan);
 
-    const updatedMealPlan = await MealPlan.findByIdAndUpdate(
-      templateId,
-      { $set: { mealSchedule: [...updatedMealSchedules, secondMealPlan] } }, 
-      { new: true, runValidators: true } 
-    );
+    const newMealPlan = await DaysMeal.create(mealPlanData);
+    const newDaysMeal = await MealPlan.create({ name, mealSchedule: newMealPlan });
 
     return res.status(201).json({
       message: "Meal Plan created successfully",
+      MealPlanTemplate: newDaysMeal,
+    });
+  } catch (error) {
+    console.error("Error creating Meal Plan:", error);
+    return res.status(500).json({
+      message: "An error occurred while creating the Meal Plan",
+      error: error.message,
+    });
+  }
+};
+
+const createVersion = async (req, res) => {
+  try {
+    const { templateId , days  } = req.body;
+    
+    const mealPlan = await MealPlan.findById(templateId).lean();
+    if (!mealPlan) return res.status(404).json({ message: "Meal Plan not found" });
+    
+    const mealIds = await createMeals();
+
+    const updatedMealSchedules = mealPlan.mealSchedule.map((schedule) => ({
+      ...schedule,
+      day: schedule.day.filter((d) => !days.includes(d)),
+    }));
+
+    const newMealPlan = await DaysMeal.create({
+      day: days,
+      meals: mealIds,
+    });
+
+    const updatedMealPlan = await MealPlan.findByIdAndUpdate(
+      templateId,
+      {
+        $set: { mealSchedule: [...updatedMealSchedules, newMealPlan] },
+      },
+      { new: true, runValidators: true }
+    );
+
+    return res.status(201).json({
+      message: "Meal Plan version created successfully",
       secondMealPlan: updatedMealPlan,
     });
   } catch (error) {
@@ -128,12 +99,9 @@ const createVersion = async (req, res) => {
   }
 };
 
-
-
 const deleteMealSchedule = async (req, res) => {
   try {
-    const templateId = req.body.templateId; // Example: "67628e1c28b7b6e0e1482782"
-    const day = req.body.day; // Example: ["Monday", "Tuesday"]
+    const { templateId , day  } = req.body;
 
     if (!Array.isArray(day) || day.length === 0) {
       return res.status(400).json({ message: "Invalid day array" });
@@ -185,50 +153,6 @@ const deleteMealSchedule = async (req, res) => {
     return res.status(500).json({ message: "Server error, please try again." });
   }
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 const getMealTemplate = async (req, res) => {
   try {
@@ -338,167 +262,5 @@ module.exports = {
   getMealTemplate,
   createVersion,
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const result = await MealPlan.aggregate([
-//   { $match: { _id: new mongoose.Types.ObjectId(newMealPlan._id) } },
-//   { $unwind: "$meal" },
-//   {
-//     $lookup: {
-//       from: "breakfasts",
-//       localField: "meal.meals",
-//       foreignField: "_id",
-//       as: "breakfastDetails",
-//     },
-//   },
-//   {
-//     $lookup: {
-//       from: "morningsnacks",
-//       localField: "meal.meals",
-//       foreignField: "_id",
-//       as: "morningSnackDetails",
-//     },
-//   },
-//   {
-//     $lookup: {
-//       from: "lunches",
-//       localField: "meal.meals",
-//       foreignField: "_id",
-//       as: "lunchDetails",
-//     },
-//   },
-//   {
-//     $lookup: {
-//       from: "afternoonsnacks",
-//       localField: "meal.meals",
-//       foreignField: "_id",
-//       as: "afternoonSnackDetails",
-//     },
-//   },
-//   {
-//     $lookup: {
-//       from: "dinners",
-//       localField: "meal.meals",
-//       foreignField: "_id",
-//       as: "dinnerDetails",
-//     },
-//   },
-//   {
-//     $lookup: {
-//       from: "suppers",
-//       localField: "meal.meals",
-//       foreignField: "_id",
-//       as: "supperDetails",
-//     },
-//   },
-//   {
-//     $addFields: {
-//       "meal.allMealDetails": {
-//         breakfast: "$breakfastDetails",
-//         morningSnack: "$morningSnackDetails",
-//         lunch: "$lunchDetails",
-//         afternoonSnack: "$afternoonSnackDetails",
-//         dinner: "$dinnerDetails",
-//         supper: "$supperDetails",
-//       },
-//     },
-//   },
-//   {
-//     $unset: "meal.meals",
-//   },
-//   {
-//     $project: {
-//       name: 1,
-//       meal: 1,
-//     },
-//   },
-// ]);
-
-
 
 
