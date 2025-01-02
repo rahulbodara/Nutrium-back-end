cfa3d4da4b25bee00f10bc8bfdd2462eff82da3c
rahulbodara <rb.globalia@gmail.com>
2025-01-02 09:11:37 +0530

    I have set data of default data.

--- src/controller/professionalpreference.js
+++ src/controller/professionalpreference.js
@@ -4,7 +4,7 @@ const User = require('../model/User');
 const createProfessionalPreference = async (userId) => {
     try {
 
-        const user = await User.findOne({_id: userId});
+        const user = await User.findOne({ _id: userId });
         const defaultData = {
             "systempreference": {
                 "timezone": "",
@@ -16,22 +16,22 @@ const createProfessionalPreference = async (userId) => {
                 "distanceunit": "Kilometer",
                 "statisticmeasure": "Percentile"
             },
-            "calendarsettings":{
+            "calendarsettings": {
                 "birtdaysystem": "Show on calendar",
-                "appointmentrequestsystem" : "Enabled"
+                "appointmentrequestsystem": "Enabled"
             },
-            "presets":{
+            "presets": {
                 "accesstomobileapp": "Enabled",
-                "clientmessages":"Enabled",
-                "clientweightregistration":"Enabled",
-                "appointementconfirmation":"Enabled",
-                "fooddiary":"Enabled",
-                "nutritionalinformation":"Enabled",
+                "clientmessages": "Enabled",
+                "clientweightregistration": "Enabled",
+                "appointementconfirmation": "Enabled",
+                "fooddiary": "Enabled",
+                "nutritionalinformation": "Enabled",
                 "clientreviews": "Enabled"
             },
-            "appointementreference":{
-                "status":"Not confirmed",
-                "appointementnotification":[
+            "appointementreference": {
+                "status": "Not confirmed",
+                "appointementnotification": [
                     {
                         "time": 1,
                         "timeunit": "hours"
@@ -46,47 +46,149 @@ const createProfessionalPreference = async (userId) => {
                     }
                 ]
             },
-            "nutritionassessmentformconfiguration":{
-                "appointementinformation":["Reason for appointment","Expectations","Clinical goals","Other information"],
-                "personalandsocialhistory":["Maternal weight gain","Maternal behaviors at pregnancy","Gestational age at birth","Bowel movements","Sleep quality",
-                "Smoker","Alcohol consumption","Marital status","Physical activity","Race","Other information"],
-                "dietaryhistory":["Breastfeeding","Food diversification","Milk and infant formulas","Usual wake up time","Usual bedtime","Types of diet",
-                "Favorite food","Disliked food","Allergies","Food intolerances","Nutritional deficiencies","Water intake","Other information"],
-                "medicalhistory":["Diseases","Medication","Personal history","Family history","Other information"],
-                "gestationalinformation":["Pregnant","Gestation type","Last menstrual period","Lactating","Beginning of lactation","Observations"],
-                "finalconsideration":["Weight","Height","Observations"]
+            "nutritionassessmentformconfiguration": {
+                "appointementinformation": ["Reason for appointment", "Expectations", "Clinical goals", "Other information"],
+                "personalandsocialhistory": ["Maternal weight gain", "Maternal behaviors at pregnancy", "Gestational age at birth", "Bowel movements", "Sleep quality",
+                    "Smoker", "Alcohol consumption", "Marital status", "Physical activity", "Race", "Other information"],
+                "dietaryhistory": ["Breastfeeding", "Food diversification", "Milk and infant formulas", "Usual wake up time", "Usual bedtime", "Types of diet",
+                    "Favorite food", "Disliked food", "Allergies", "Food intolerances", "Nutritional deficiencies", "Water intake", "Other information"],
+                "medicalhistory": ["Diseases", "Medication", "Personal history", "Family history", "Other information"],
+                "gestationalinformation": ["Pregnant", "Gestation type", "Last menstrual period", "Lactating", "Beginning of lactation", "Observations"],
+                "finalconsideration": ["Weight", "Height", "Observations"]
             },
-            "emailandprintingpreference":{
-                "mealplanemailsubject":"Meal plan",
-                "mealplanemailbody":`Hello [patient_first_name]. The meal plan created on [meal_plan_last_update_date] is sent in attachment. \
+            "emailandprintingpreference": {
+                "mealplanemailsubject": "Meal plan",
+                "mealplanemailbody": `Hello [patient_first_name]. The meal plan created on [meal_plan_last_update_date] is sent in attachment. \
                 Remember that you can check the meal plan in the mobile app, available for Android and iOS. \
                 \
                 I'm available to help you with any question you have. \
                 \
                 Best regards,${user.fullName} `,
-                "mealplansections":["Client information","Meal plan","Recommendations","Other information","Recipes","Signature"],
-                "printingdesign":"Simple design (2018)",
-                "multipledaysprintinglayout":"Layout with columns",
-                "fontsizeoftheprints":"Normal"
+                "mealplansections": ["Client information", "Meal plan", "Recommendations", "Other information", "Recipes", "Signature"],
+                "printingdesign": "Simple design (2018)",
+                "multipledaysprintinglayout": "Layout with columns",
+                "fontsizeoftheprints": "Normal"
             },
-            "mealplansettings":{
-                "lunchanddinnersection":["Appetizer","Dish","Dessert","Beverage"],
-                "fooddatabases":["USDA, 2018","CNF, GC","McCance and Widdowson's"]
+            "mealplansettings": {
+                "lunchanddinnersection": ["Appetizer", "Dish", "Dessert", "Beverage"],
+                "fooddatabases": ["USDA, 2018", "CNF, GC", "McCance and Widdowson's"]
             },
-            "measurementspreferences":{
-                "deducemeasurements":"Deduce fat mass, lean body mass and percentages",
-                "anthropometricmeasurements":["Weight","Height","Hip circumference","Waist circumference"],
-                "analyticaldata":["Diastolic blood pressure","HDL Cholesterol","LDL Cholesterol","Systolic blood pressure","Total cholesterol","Triglycerides"],
-                "bodycompotion":["Body fat percentage","Fat mass","Muscle mass","Muscle mass percentage"]
+            "measurementspreferences": {
+                "deducemeasurements": "Deduce fat mass, lean body mass and percentages",
+                "anthropometricmeasurements": [{
+                    text: "Weight",
+                    unit: [
+                        {
+                            name: "Kilogram",
+                            value: "kg",
+                        },
+                        {
+                            name: "Pound",
+                            value: "pounds",
+                        },
+                        {
+                            name: "Stone",
+                            value: "stone",
+                        },
+                    ]
+                }, {
+                    text: "Height",
+                    unit: [
+                        {
+                            name: "Centimetres",
+                            value: "cm",
+                        },
+                        {
+                            name: "Foots and inches",
+                            value: "foot and inches",
+                        }
+                    ]
+                }, {
+                    text: "Hip circumference",
+                    unit: [
+                        {
+                            name: "centimeters",
+                            value: "cm",
+                        },
+                        {
+                            name: "Inch",
+                            value: "inch",
+                        }
+                    ]
+                }, {
+                    text: "Waist circumference",
+                    unit: [
+                        {
+                            name: "centimeters",
+                            value: "cm",
+                        },
+                        {
+                            name: "Inch",
+                            value: "inch",
+                        }
+                    ]
+                }],
+                "analyticaldata": [{
+                    text: "Diastolic blood pressure",
+                    unit: [
+                        { name: "Millimeters of Mercury", value: "mmHg" }
+                    ]
+                }, {
+                    text: "HDL Cholesterol",
+                    unit: [
+                        { name: "Milligrams per Deciliter", value: "mg/dL" }
+                    ]
+                }, {
+                    text: "LDL Cholesterol",
+                    unit: [
+                        { name: "Milligrams per Deciliter", value: "mg/dL" }
+                    ]
+                }, {
+                    text: "Systolic blood pressure",
+                    unit: [
+                        { name: "Millimeters of Mercury", value: "mmHg" }
+                    ]
+                }, {
+                    text: "Total cholesterol",
+                    unit: [
+                        { name: "Milligrams per Deciliter", value: "mg/dL" }
+                    ]
+                }, {
+                    text: "Triglycerides",
+                    unit: [
+                        { name: "Milligrams per Deciliter", value: "mg/dL" }
+                    ]
+                }],
+                "bodycompotion": [{
+                    text: "Body fat percentage",
+                    unit: [
+                        { name: "Percentage", value: "%" }
+                    ]
+                }, {
+                    text: "Fat mass",
+                    unit: [
+                        { name: "Kilograms", value: "kg" }
+                    ]
+                }, {
+                    text: "Muscle mass",
+                    unit: [
+                        { name: "Kilograms", value: "kg" }
+                    ]
+                }, {
+                    text: "Muscle mass percentage",
+                    unit: [
+                        { name: "Percentage", value: "%" }
+                    ]
+                }]
             }
         }
 
-            const preference = new professionalPreference({
-                userId: userId,
-                ...defaultData
-            });
+        const preference = new professionalPreference({
+            userId: userId,
+            ...defaultData
+        });
 
-            const result = await preference.save();
+        const result = await preference.save();
 
     } catch (error) {
         console.log(error);
@@ -129,23 +231,21 @@ const updateprofessionPreference = async (req, res, next) => {
                 }
                 if (existingPreference.appointementreference.hasOwnProperty(field)) {
                     const appointementreference = existingPreference.appointementreference;
-                
+
                     if (updateFields.hasOwnProperty('status')) {
                         appointementreference.status = updateFields.status;
                     }
-                
+
                     if (updateFields.hasOwnProperty('appointementnotification')) {
                         const updatedAppointementNotifications = updateFields.appointementnotification;
-                
+
                         if (Array.isArray(appointementreference.appointementnotification)) {
                             updatedAppointementNotifications.forEach(updatedNotification => {
-                                const indexToUpdate = appointementreference.appointementnotification.findIndex((oldNotification) =>  
-                                    
-                                     {
-                                        if(oldNotification._id.toString() === updatedNotification._id){
-                                            return true
-                                        }
-                                     }
+                                const indexToUpdate = appointementreference.appointementnotification.findIndex((oldNotification) => {
+                                    if (oldNotification._id.toString() === updatedNotification._id) {
+                                        return true
+                                    }
+                                }
                                 );
                                 if (indexToUpdate !== -1) {
                                     console.log(indexToUpdate);
@@ -155,16 +255,16 @@ const updateprofessionPreference = async (req, res, next) => {
                         }
                     }
                 }
-                if (existingPreference.nutritionassessmentformconfiguration.hasOwnProperty(field)){
+                if (existingPreference.nutritionassessmentformconfiguration.hasOwnProperty(field)) {
                     existingPreference.nutritionassessmentformconfiguration[field] = updateFields[field];
                 }
-                if (existingPreference.emailandprintingpreference.hasOwnProperty(field)){
+                if (existingPreference.emailandprintingpreference.hasOwnProperty(field)) {
                     existingPreference.emailandprintingpreference[field] = updateFields[field];
                 }
-                if (existingPreference.mealplansettings.hasOwnProperty(field)){
+                if (existingPreference.mealplansettings.hasOwnProperty(field)) {
                     existingPreference.mealplansettings[field] = updateFields[field];
                 }
-                if (existingPreference.measurementspreferences.hasOwnProperty(field)){
+                if (existingPreference.measurementspreferences.hasOwnProperty(field)) {
                     existingPreference.measurementspreferences[field] = updateFields[field];
                 }
             }
