fd7f77bf99a107c26bb1948a0dbb85ad7e6318c9
rahulbodara <rb.globalia@gmail.com>
2025-03-26 12:28:10 +0530

    change in recommendation api

--- src/controller/recommendation.js
+++ src/controller/recommendation.js
@@ -766,7 +766,7 @@ const getFoodAvoid = async (req, res) => {
         );
 
         if (!clientData) {
-            return res.status(404).json({ success: true, message: "No records found for this client." });
+            return res.status(200s).json({ success: true, message: "No records found for this client.", data: [] });
         }
 
 
